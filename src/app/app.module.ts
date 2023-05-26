8809107269ea1243878c81763a2328b9a457cc45
Antarach <antara.chowdhury@emids.com>
2023-05-26 14:10:05 +0530

    login and signin in template driven form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,29 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
-//import { FormControl, FormGroup } from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule,FormsModule } from '@angular/forms';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatIconModule} from '@angular/material/icon';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SignupComponent } from './signup/signup.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    SignupComponent,
+    RegistrationComponent,
+    
   ],
   imports: [
     BrowserModule,
@@ -29,8 +40,12 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatButtonModule,
     MatCardModule,
     MatToolbarModule,
-    ReactiveFormsModule
-   // FormControl, FormGroup
+    ReactiveFormsModule,
+    MatTabsModule,
+    FormsModule,
+    MatCheckboxModule,
+    MatIconModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,35 +1,29 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
 })
-export class LoginComponent {
+export class SignupComponent {
   signUpUsers: any[] = [];
-  signUpForm:any = {
+  signUpForm: any = {
     firstname: '',
     lastname: '',
     email: '',
     password: '',
     confirmPassword: '',
   };
-  logInForm:any={
-    email:'',
-    password:''
-  }
+ 
   constructor() { }
-
   ngOnInit(): void {
     const localData = localStorage.getItem('signUpUsers');
     if (localData != null) {
       this.signUpUsers = JSON.parse(localData);
     }
   }
-  signUpUser(value: any) {
-    console.log(value)
+  signUpUser(value:any) {
+    console.log()
     this.signUpUsers.push(this.signUpForm);
     localStorage.setItem('signUpUsers', JSON.stringify(this.signUpUsers));
     this.signUpForm = {
@@ -38,16 +32,6 @@ export class LoginComponent {
       email: '',
       password: '',
       confirmPassword: ''
-    }
-  }
-  logInUser(value: any) {
-    console.log(value)
-    const isUserExist = this.signUpUsers.find(m => m.email == this.logInForm.email && m.password == this.logInForm.password);
-    if (isUserExist) {
-      alert("User Login Successfully....")
-    }
-    else {
-      alert("Wrong Credential!")
     }
   }
 }
